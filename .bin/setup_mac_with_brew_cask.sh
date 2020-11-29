4aa7301d4ffcbb44593d136d582fd5725c1f7f32
inductor <kohei.ota@hpe.com>
2020-11-29 22:55:54 +0900

    update

--- .bin/setup_mac_with_brew_cask.sh
+++ .bin/setup_mac_with_brew_cask.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 # brew cask install google-chrome docker discord visual-studio-code
+if [ -f /etc/lsb-release ]; then
+    echo 'Ubuntu detected. Installing dependency.'
+    sudo apt-get update && sudo apt-get install -y build-essential locales-all git
+    sudo chmod +x /usr/share/doc/git/contrib/diff-highlight/diff-highlight
+    sudo ln -s /usr/share/doc/git/contrib/diff-highlight/diff-highlight /usr/local/bin/diff-highlight
+fi
 brew tap instrumenta/instrumenta
 brew install make anyenv bash bash-completion cfssl conftest direnv goodwithtech/dockle doctl eksctl fzf gh ghq git helm hub hugo kind kubectx kustomize minikube mkcert neovim shellcheck starship datawire/blackbird/telepresence tfenv tflint tig aquasecurity/trivy/trivy wget k9s
 
