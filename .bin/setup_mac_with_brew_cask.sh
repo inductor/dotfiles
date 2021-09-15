2f0ba55b46101abf140474f217ddf6ec6ddfff34
inductor <kohei.ota@hpe.com>
2021-09-15 20:34:13 +0900

    add krew install

--- .bin/setup_mac_with_brew_cask.sh
+++ .bin/setup_mac_with_brew_cask.sh
@@ -12,4 +12,18 @@ brew tap  weaveworks/tap
 brew install make anyenv bash bash-completion cfssl conftest direnv goodwithtech/r/dockle doctl weaveworks/tap/eksctl fzf gh ghq git helm hub hugo kind kubectx kustomize minikube mkcert neovim shellcheck starship datawire/blackbird/telepresence tfenv tflint tig aquasecurity/trivy/trivy wget k9s
 curl -fsL https://golang.org/dl/go1.15.6.linux-amd64.tar.gz | sudo tar -C /usr/local -xzf -
 
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/krew.tar.gz" &&
+  tar zxvf krew.tar.gz &&
+  KREW=./krew-"${OS}_${ARCH}" &&
+  "$KREW" install krew
+)
+
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+
+kubectl krew install ctx ns iexec status neat view-secret images rolesum open-svc tree exec-as modify-secret view-serviceaccount-kubeconfig get-all node-shell ca-cert who-can df-pv resource-capacity fleet prompt ingress-nginx operator cert-manager deprecations
+
 anyenv install --init
